1062c65ba636c8bcf0c8dc589c26d38bbe7d597a
晨魔 <139463472+MorningDemon515@users.noreply.github.com>
2026-07-16 12:38:56 +0800

    Remove Elements

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 
 typedef enum Type
 {
-	T_Vector,
-	T_Book
+	VECTOR,
+	BOOK
 } Type;
 
 typedef struct ElementRecord {
+	int id;
     Type type;
     int offset;    
 } ElementRecord;
@@ -31,23 +32,45 @@ typedef struct Manager
 	int total;
 	
 	ElementRecord* records;
+	int nextId;
 } Manager;
 
+typedef struct Handle {
+    int id;
+} Handle;
+
 Manager* InitManager();
-Vector CreateVector(Manager* manager);
-Book CreateBook(Manager* manager);
+Handle CreateVector(Manager* manager);
+Handle CreateBook(Manager* manager);
 void ReleaseManager(Manager* manager);
+void* GetElements(Manager* manager, Handle handle);
+void RemoveElements(Manager* manager, Handle handle);
 
 int main()
 {
-
     Manager* manager = InitManager();
 	
-	Vector v = CreateVector(manager);
-	Book book = CreateBook(manager);
+	Handle _1 = CreateVector(manager);
+	Handle _2 = CreateVector(manager);
+	Handle _3 = CreateBook(manager);
 	
-	printf("%f, %f, %f, %f\n", v.x, v.y, v.z,v.w);
-	printf("number: %d, name: %s \n", book.number, book.name);
+    Vector v = *(Vector*)GetElements(manager, _1);
+	Vector v1 = *(Vector*)GetElements(manager, _2);
+	Book book = *(Book*)GetElements(manager, _3);
+	printf("Before removal:\n");
+	printf("v : %f, %f, %f, %f\n", v.x, v.y, v.z, v.w);
+	printf("v1: %f, %f, %f, %f\n", v1.x, v1.y, v1.z, v1.w);
+	printf("book: number: %d, name: %s \n\n", book.number, book.name);
+	
+	RemoveElements(manager, _2);
+
+	Book* book_after = (Book*)GetElements(manager, _3);
+	if (book_after != NULL) {
+		printf("After removal:\n");
+		printf("book: number: %d, name: %s \n\n", book_after->number, book_after->name);
+	} else {
+		printf("Book not found after removal!\n\n");
+	}
 	
     ReleaseManager(manager);
 	system("pause");
@@ -61,10 +84,11 @@ Manager* InitManager()
 	manager->records = NULL;
 	manager->size = 0;
 	manager->total = 0;
+	manager->nextId = 0;
 	return manager;
 }
 
-Vector CreateVector(Manager* manager)
+Handle CreateVector(Manager* manager)
 {
 	int currentSize = manager->size;
 	int currentTotal = manager->total;
@@ -86,7 +110,9 @@ Vector CreateVector(Manager* manager)
         exit(1);
     }
     manager->records = newRecords;
-    manager->records[manager->size].type = T_Vector;
+	int myId = manager->nextId++;
+    manager->records[manager->size].id = myId;
+    manager->records[manager->size].type = VECTOR;
     manager->records[manager->size].offset = currentTotal;
 	
 	manager->size += 1;
@@ -101,10 +127,12 @@ Vector CreateVector(Manager* manager)
 	void* targetAddr = (char*)manager->data + currentTotal;
 	memcpy(targetAddr, &temp, sizeof(temp));
 	
-	return *(Vector*)targetAddr;
+	Handle h = { myId };
+	
+	return h;
 }
 
-Book CreateBook(Manager* manager)
+Handle CreateBook(Manager* manager)
 {
 	int currentSize = manager->size;
 	int currentTotal = manager->total;
@@ -126,7 +154,9 @@ Book CreateBook(Manager* manager)
         exit(1);
     }
     manager->records = newRecords;
-    manager->records[manager->size].type = T_Book;      
+	int myId = manager->nextId++;
+    manager->records[manager->size].id = myId;
+    manager->records[manager->size].type = BOOK;      
     manager->records[manager->size].offset = currentTotal; 
 	
 	manager->size += 1;
@@ -139,7 +169,85 @@ Book CreateBook(Manager* manager)
 	void* targetAddr = (char*)manager->data + currentTotal;
 	memcpy(targetAddr, &temp, sizeof(temp));
 	
-	return *(Book*)targetAddr;
+	Handle h = { myId };
+	
+	return h;
+}
+
+void* GetElements(Manager* manager, Handle handle)
+{
+	for (int i = 0; i < manager->size; i++) {
+        if (manager->records[i].id == handle.id) {
+            return (char*)manager->data + manager->records[i].offset;
+        }
+    }
+    return NULL;
+}
+
+void RemoveElements(Manager* manager, Handle handle)
+{
+	int targetIndex = -1;
+	
+	for (int i = 0; i < manager->size; i++) {
+        if (manager->records[i].id == handle.id) {
+			targetIndex = i;
+			break;
+        }
+    }
+	
+	if (targetIndex == -1) {
+		printf("Handle not found!\n");
+		return;
+	}
+
+	Type targetType = manager->records[targetIndex].type;
+	int targetOffset = manager->records[targetIndex].offset;
+	int targetSize = 0;
+	
+	switch (targetType)
+	{
+		case VECTOR:
+		    targetSize = sizeof(Vector);
+			printf("[Removal] Releasing Vector at offset %d\n", targetOffset);
+		    break;
+		
+		case BOOK:
+		    targetSize = sizeof(Book);
+			printf("[Removal] Releasing Book at offset %d\n", targetOffset);
+		    break;
+			
+		default:
+            break;		
+	}
+
+	int moveBytes = manager->total - (targetOffset + targetSize);
+	if (moveBytes > 0) {
+		void* dest = (char*)manager->data + targetOffset;
+		void* src = (char*)manager->data + targetOffset + targetSize;
+		memmove(dest, src, moveBytes);
+	}
+	
+	for (int i = targetIndex; i < manager->size - 1; i++) {
+		manager->records[i] = manager->records[i + 1];
+		manager->records[i].offset -= targetSize;
+	}
+	
+	manager->size -= 1;
+	manager->total -= targetSize;
+
+	if (manager->total > 0) {
+		manager->data = realloc(manager->data, manager->total);
+	} else {
+		free(manager->data);
+		manager->data = NULL;
+	}
+	
+	if (manager->size > 0) {
+		manager->records = realloc(manager->records, manager->size * sizeof(ElementRecord));
+	} else {
+		free(manager->records);
+		manager->records = NULL;
+	}
 }
 
 void ReleaseManager(Manager* manager)
@@ -151,11 +259,11 @@ void ReleaseManager(Manager* manager)
         void* itemAddr = (char*)manager->data + manager->records[i].offset;
         
         switch (manager->records[i].type) {
-            case T_Book:
+            case BOOK:
                 printf("Releasing Book at offset %d\n", manager->records[i].offset);
                 break;
                 
-            case T_Vector:
+            case VECTOR:
 			    printf("Releasing Vector at offset %d\n", manager->records[i].offset);
                 break;
             
