c21857d9dc6d3f8218246b2f36cf0e17d9fdff45
晨魔 <139463472+MorningDemon515@users.noreply.github.com>
2026-07-19 23:50:18 +0800

    Update ~~~

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef enum Type
 typedef struct ElementRecord {
 	int id;
     Type type;
-    int offset;    
+    void* ptr; 
 } ElementRecord;
 
 typedef struct Vector
@@ -27,10 +27,7 @@ typedef struct Book
 
 typedef struct Manager
 {
-	void* data;
 	int size;
-	int total;
-	
 	ElementRecord* records;
 	int nextId;
 } Manager;
@@ -54,13 +51,13 @@ int main()
 	Handle _2 = CreateVector(manager);
 	Handle _3 = CreateBook(manager);
 	
-    Vector v = *(Vector*)GetElements(manager, _1);
-	Vector v1 = *(Vector*)GetElements(manager, _2);
-	Book book = *(Book*)GetElements(manager, _3);
+    Vector* v = (Vector*)GetElements(manager, _1);
+	Vector* v1 = (Vector*)GetElements(manager, _2);
+	Book* book = (Book*)GetElements(manager, _3);
 	printf("Before removal:\n");
-	printf("v : %f, %f, %f, %f\n", v.x, v.y, v.z, v.w);
-	printf("v1: %f, %f, %f, %f\n", v1.x, v1.y, v1.z, v1.w);
-	printf("book: number: %d, name: %s \n\n", book.number, book.name);
+	printf("v : %f, %f, %f, %f\n", v->x, v->y, v->z, v->w);
+	printf("v1: %f, %f, %f, %f\n", v1->x, v1->y, v1->z, v1->w);
+	printf("book: number: %d, name: %s \n\n", book->number, book->name);
 	
 	RemoveElements(manager, _2);
 
@@ -79,53 +76,42 @@ int main()
 
 Manager* InitManager()
 {
-	Manager* manager =malloc(sizeof(Manager));
-	manager->data = NULL;
-	manager->records = NULL;
-	manager->size = 0;
-	manager->total = 0;
-	manager->nextId = 0;
+	Manager* manager = malloc(sizeof(Manager));
+	
+	if (manager != NULL)
+	{
+        manager->records = NULL;
+	    manager->size = 0;
+	    manager->nextId = 0;		
+	}
+	
 	return manager;
 }
 
 Handle CreateVector(Manager* manager)
 {
-	int currentSize = manager->size;
-	int currentTotal = manager->total;
-	
-	void* data = realloc(manager->data ,manager->total + sizeof(Vector));
-	
-	if (data == NULL) {
-    printf("Failed! \n");
-    ReleaseManager(manager);
-    exit(1); 
-    }
-	
-	manager->data = data;
-	
+
+    Vector* temp = malloc(sizeof(Vector));
+	temp->x = 1.0f;
+	temp->y = 0.0f;
+	temp->z = 1.0f;
+	temp->w = 0.0f;
+
 	ElementRecord* newRecords = realloc(manager->records, (manager->size + 1) * sizeof(ElementRecord));
     if (newRecords == NULL) {
         printf("Failed to allocate records! \n");
         ReleaseManager(manager);
         exit(1);
     }
+	
     manager->records = newRecords;
+	
 	int myId = manager->nextId++;
     manager->records[manager->size].id = myId;
     manager->records[manager->size].type = VECTOR;
-    manager->records[manager->size].offset = currentTotal;
+	manager->records[manager->size].ptr = temp;
 	
 	manager->size += 1;
-	manager->total += sizeof(Vector);
-	
-	Vector temp;
-	temp.x = 1.0f;
-	temp.y = 2.0f;
-	temp.z = 3.0f;
-	temp.w = 4.0f;
-	
-	void* targetAddr = (char*)manager->data + currentTotal;
-	memcpy(targetAddr, &temp, sizeof(temp));
 	
 	Handle h = { myId };
 	
@@ -134,40 +120,26 @@ Handle CreateVector(Manager* manager)
 
 Handle CreateBook(Manager* manager)
 {
-	int currentSize = manager->size;
-	int currentTotal = manager->total;
 	
-	void* data = realloc(manager->data ,manager->total + sizeof(Book));
-	
-	if (data == NULL) {
-    printf("Failed! \n");
-    ReleaseManager(manager);
-    exit(1); 
-    }
-	
-	manager->data = data;
-	
+	Book* temp = malloc(sizeof(Book));
+	temp->number = 10;
+	temp->name = "A course of pure math";
+
 	ElementRecord* newRecords = realloc(manager->records, (manager->size + 1) * sizeof(ElementRecord));
     if (newRecords == NULL) {
         printf("Failed to allocate records! \n");
         ReleaseManager(manager);
         exit(1);
     }
+	
     manager->records = newRecords;
+	
 	int myId = manager->nextId++;
     manager->records[manager->size].id = myId;
     manager->records[manager->size].type = BOOK;      
-    manager->records[manager->size].offset = currentTotal; 
+	manager->records[manager->size].ptr = temp;
 	
 	manager->size += 1;
-	manager->total += sizeof(Book);
-	
-	Book temp;
-    temp.number = 1;
-	temp.name = "the math course of pure";
-	
-	void* targetAddr = (char*)manager->data + currentTotal;
-	memcpy(targetAddr, &temp, sizeof(temp));
 	
 	Handle h = { myId };
 	
@@ -178,7 +150,7 @@ void* GetElements(Manager* manager, Handle handle)
 {
 	for (int i = 0; i < manager->size; i++) {
         if (manager->records[i].id == handle.id) {
-            return (char*)manager->data + manager->records[i].offset;
+            return manager->records[i].ptr;
         }
     }
     return NULL;
@@ -201,49 +173,39 @@ void RemoveElements(Manager* manager, Handle handle)
 	}
 
 	Type targetType = manager->records[targetIndex].type;
-	int targetOffset = manager->records[targetIndex].offset;
-	int targetSize = 0;
+	void* itemAddr = manager->records[targetIndex].ptr;
 	
-	switch (targetType)
+	if (itemAddr != NULL)
 	{
+		switch (targetType)
+	    {
 		case VECTOR:
-		    targetSize = sizeof(Vector);
-			printf("[Removal] Releasing Vector at offset %d\n", targetOffset);
+		    free(itemAddr);
+			printf("[Removal] Releasing Vector, Handle: %d\n", manager->records[targetIndex].id);
 		    break;
 		
 		case BOOK:
-		    targetSize = sizeof(Book);
-			printf("[Removal] Releasing Book at offset %d\n", targetOffset);
+		    free(itemAddr);
+			printf("[Removal] Releasing Book, Handle: %d\n", manager->records[targetIndex].id);
 		    break;
 			
 		default:
             break;		
-	}
-
-	int moveBytes = manager->total - (targetOffset + targetSize);
-	if (moveBytes > 0) {
-		void* dest = (char*)manager->data + targetOffset;
-		void* src = (char*)manager->data + targetOffset + targetSize;
-		memmove(dest, src, moveBytes);
+	    }
 	}
 	
 	for (int i = targetIndex; i < manager->size - 1; i++) {
 		manager->records[i] = manager->records[i + 1];
-		manager->records[i].offset -= targetSize;
 	}
 	
 	manager->size -= 1;
-	manager->total -= targetSize;
-
-	if (manager->total > 0) {
-		manager->data = realloc(manager->data, manager->total);
-	} else {
-		free(manager->data);
-		manager->data = NULL;
-	}
 	
 	if (manager->size > 0) {
-		manager->records = realloc(manager->records, manager->size * sizeof(ElementRecord));
+		void* temp = realloc(manager->records, manager->size * sizeof(ElementRecord));
+		if (temp != NULL)
+		{
+			manager->records = temp;
+		}
 	} else {
 		free(manager->records);
 		manager->records = NULL;
@@ -256,15 +218,17 @@ void ReleaseManager(Manager* manager)
 
     for (int i = 0; i < manager->size; i++) {
 
-        void* itemAddr = (char*)manager->data + manager->records[i].offset;
+        void* itemAddr = manager->records[i].ptr;
         
         switch (manager->records[i].type) {
             case BOOK:
-                printf("Releasing Book at offset %d\n", manager->records[i].offset);
+                free(itemAddr);
+				printf("Released Book, Handle: %d\n", manager->records[i].id);
                 break;
                 
             case VECTOR:
-			    printf("Releasing Vector at offset %d\n", manager->records[i].offset);
+			    free(itemAddr);
+				printf("Released VECTOR, Handle: %d\n", manager->records[i].id);
                 break;
             
             default:
@@ -273,6 +237,5 @@ void ReleaseManager(Manager* manager)
     }
 
     free(manager->records); 
-    free(manager->data);  
     free(manager);       
 }
